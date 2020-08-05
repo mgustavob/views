a12719b94e9ea6e13d14e2e2e0e65ceb2070d903
Martin Briceno <martin.briceno@live.com>
2020-08-05 16:55:35 -0400

    Fixed a few bugs

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send("This is the Home Page!")
-    res.sendFile(__dirname+'views/index.html')
+    //res.send("This is the Home Page!")
+    res.sendFile(__dirname+'/views/index.html')
+    // res.sendFile(`${__dirname}/views/index.html`)
 })
 
 app.get('/about', (req, res)=>{
-    res.send('Some Stuff about will go here')
-    res.sendFile(__dirname+'views/about.html')
+    //res.send('Some Stuff about will go here')
+    res.sendFile(__dirname+'/views/about.html')
 })
 
 app.get('/blog', (req, res)=>{
-    res.send('A directory of all my blog posts will go here.')
-    res.sendFile(__dirname+'views/blog-directory.html')
+    //res.send('A directory of all my blog posts will go here.')
+    res.sendFile(__dirname+'/views/blog-directory.html')
 })
 
 app.listen(8000, ()=>{
-    console.log("listening to port 8000")
+    //console.log("listening to port 8000")
 })
+
+//app.listen(8000) works
